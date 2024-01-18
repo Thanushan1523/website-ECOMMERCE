a12c88992a5931c53f83421939f88184bf9cf0bf
Thanushan1523 <thanushan1523@gmail.com>
2024-01-18 09:25:34 -0500

    adding seetings-for autoemail

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -114,6 +114,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+# sending email
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_HOST_USER ='your email'
+EMAIL_HOST_PASSWORD ='your email password'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -131,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGES_TAGS = {
     messages.ERROR: 'danger'
 }
+
+

e947f0269a0a004419ca5bb25224c61a4e6e2cb5
Thanushan1523 <thanushan1523@gmail.com>
2024-01-20 19:10:08 -0500

    developing login page

--- authapp/views.py
+++ authapp/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
+from django.template.loader_tags import render_to_string
+from django.utils.http import urlsafe_base64_decode , urlsafe_base64_encode
+from .utils import TokenGenerator , generate_token
 # Create your views here.
 def signup(request):
    
@@ -17,13 +19,21 @@ def signup(request):
             return redirect('/auth/signup/')
         try:
             if User.objects.get(username=email):
-                messages.warning(request,"email is taken")
+                messages.info(request,"email is taken")
                 return HttpResponse("usercreated")
                 return redirect('/auth/signup/')
         except Exception as identifier :
             pass
         user=User.objects.create_user(email,email,password)
+        user.is_active=False
+
         user.save()
+        email_subject="Active Your Account "
+        message =render_to_string('activate.html',{'user':user,
+                                                        'domain':'127.0.0.1:8000',
+                                                        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                                                        'token':generate_token.make_token(user)
+                                                        })
         messages.success(request,'USER IS CREATED PLEASE LOGIN')
         return HttpResponse("usercreated")
         return redirect('/auth/login/')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -115,9 +115,9 @@ USE_I18N = True
 USE_TZ = True
 
 # sending email
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_HOST_USER ='your email'
-EMAIL_HOST_PASSWORD ='your email password'
+EMAIL_HOST ='smtp.secureserver.net'
+EMAIL_HOST_USER ='thanu1523@yahoo.com'
+EMAIL_HOST_PASSWORD ='150319901704'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'

6563af83774858307df3695e7d2f7d58267fae36
Thanushan1523 <thanushan1523@gmail.com>
2024-01-22 21:33:50 -0500

    developing login page

--- authapp/views.py
+++ authapp/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from .utils import TokenGenerator, generate_token
-from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
+from django.utils.encoding import force_bytes,force_str, DjangoUnicodeDecodeError
 
 from django.core.mail import EmailMessage
 from django.conf import settings
@@ -36,7 +36,7 @@ def signup(request):
         email_subject = "Activate Your Account"
         message = render_to_string('activate.html', {
             'user': user,
-            'domain': '127.0.0.1:8000',
+            'domain': '127.0.0.1:8000/',
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
             'token': TokenGenerator().make_token(user)
         })
@@ -55,7 +55,7 @@ def signup(request):
 class ActivateAccountView(View):
     def get(self, request, uidb64, token):
         try:
-            uid = force_text(urlsafe_base64_decode(uidb64))
+            uid = force_str(urlsafe_base64_decode(uidb64))
             user = User.objects.get(pk=uid)
         except (TypeError, ValueError, DjangoUnicodeDecodeError, User.DoesNotExist):
             user = None
@@ -71,7 +71,7 @@ class ActivateAccountView(View):
 
 
 def handlelogin(request):
-    return render(request, "login.html")
+    return render(request, "authentication/login.html")
 
 
 def handlelogout(request):
